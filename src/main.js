b89bbc1dbf764015d41599797200c14820ad719d
GoTime0726 <840802783@qq.com>
2020-12-15 18:02:33 +0800

    'master分支提交'

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './assets/css/global.css'
 import axios from 'axios'
 import './utils/directive'
 import TreeTable from 'vue-table-with-tree-grid'
-// 配置请求根路径
+// 配置请求根路径merge
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // vue实例都有这个方法
 axios.interceptors.request.use(config => {
